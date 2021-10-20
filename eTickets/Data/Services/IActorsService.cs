c1a0996f4df406daa18acf57bd2071a3f83ac2f0
Jane Moroz <Moroz.jane@yahoo.com>
2021-10-20 18:34:53 +0300

    Created ActorsService

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,44 @@
+﻿using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        /// <summary>
+        /// Get all actors
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<Actor>> GetAll();
+
+        /// <summary>
+        /// Get an actor by id
+        /// </summary>
+        /// <param name="id">Actor's Id</param>
+        /// <returns></returns>
+        Actor GetById(int id);
+
+        /// <summary>
+        /// Add a new actor
+        /// </summary>
+        /// <param name="actor">Actor's model</param>
+        void Add(Actor actor);
+
+        /// <summary>
+        /// Update actor
+        /// </summary>
+        /// <param name="id">Actor's id</param>
+        /// <param name="newActor"></param>
+        /// <returns></returns>
+        Actor Update(int id, Actor newActor);
+
+        /// <summary>
+        /// Delete actor
+        /// </summary>
+        /// <param name="id">Actor's id</param>
+        void Delete(int id);
+    }
+}
