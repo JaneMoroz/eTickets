048717f1bb4a6aa620fd12bffd7379b5cb5bac44
Jane Moroz <Moroz.jane@yahoo.com>
2021-10-20 17:31:12 +0300

    Added the movies/actors/directors controllers and views

---
+++ eTickets/Controllers/DirectorsController.cs
@@ -0,0 +1,26 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class DirectorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public DirectorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allDirectors = await _context.Directors.ToListAsync();
+            return View(allDirectors);
+        }
+    }
+}
