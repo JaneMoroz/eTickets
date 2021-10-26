875f87df0905d3fe2bdb02147b5942b1d0cf8bbe
Jane Moroz <Moroz.jane@yahoo.com>
2021-10-26 14:03:16 +0300

    (configurations)

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,10 +1,13 @@
 using eTickets.Data;
 using eTickets.Data.Cart;
 using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,7 +46,14 @@ namespace eTickets
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
 
+            // Authentication and Authorization configuration
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+            services.AddMemoryCache();
             services.AddSession();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            });
 
             services.AddControllersWithViews();
         }
@@ -69,6 +79,8 @@ namespace eTickets
 
             app.UseSession();
 
+            // Authentication and Authorization
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -80,6 +92,7 @@ namespace eTickets
 
             //Seed Database
             AppDbInitializer.Seed(app);
+            AppDbInitializer.SeedUsersAndRoles(app).Wait();
         }
     }
 }
