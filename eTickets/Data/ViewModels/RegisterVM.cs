d68b01783740be8e7ceb277000cfe97492a63f4f
Jane Moroz <Moroz.jane@yahoo.com>
2021-10-26 15:56:17 +0300

    Added And Wired Up Register View

---
+++ eTickets/Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email")]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
